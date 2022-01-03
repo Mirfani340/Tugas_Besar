3002bbca995ec37adc808632a26c0fce6222ea84
alandmyt <92350338+alandmyt@users.noreply.github.com>
2022-01-03 09:22:11 +0700

    Tugas_Besar

---
+++ src/Contoh2/Keperluan.java
@@ -0,0 +1,5 @@
+package Contoh2;
+
+public abstract class Keperluan {
+    int head;
+}
---
+++ src/Contoh2/Queue.java
@@ -0,0 +1,52 @@
+package Contoh2;
+
+import java.util.Scanner;
+
+public class Queue extends Keperluan {
+
+    String[] data;
+    int head = 0;
+    int flag = 0;
+
+    public Queue(int jumlah) {
+        data = new String[jumlah];
+    }
+
+    public void enqueue() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nMasukan Nama : ");
+        String inputan = sc.nextLine();
+        if (head < data.length) {
+            data[head] = inputan;
+            head++;
+        } else {
+            System.out.println("Queue is full");
+        }
+    }
+
+    public String dequeue() {
+        String tmp = data[0];
+        for (int i = 0; i < data.length - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        head--;
+        if (head >= 0) {
+            data[head] = null;
+            flag++;
+        } else {
+            flag = 0;
+            head = 0;
+            return "Queue is empty";
+        }
+        return "No.00" + flag + ", Nama : " + tmp;
+    }
+
+    public void print() {
+        if (data[0] == null){
+            System.out.println("Antrian kosong");
+        } else
+        for (int i = 0; i < data.length; i++) {
+            System.out.println("No.00" + (flag++) + ", Nama : " + data[i]);
+        }
+    }
+}
